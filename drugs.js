40ee1727731d4aaf43ae392000aae541a031b3d7
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-11 20:27:02 +0300

    Update drugs.js

--- drugs.js
+++ drugs.js
@@ -1,1 +1,20 @@
-const drugs = [];
+const drugs = [
+  {
+    id: "paracetamol",
+    name: "Paracetamol",
+    dosePerKg: 15,
+    unit: "mg/kg/dose"
+  },
+  {
+    id: "ibuprofen",
+    name: "Ibuprofen",
+    dosePerKg: 10,
+    unit: "mg/kg/dose"
+  },
+  {
+    id: "amoxicillin",
+    name: "Amoxicillin",
+    dosePerKg: 25,
+    unit: "mg/kg/dose"
+  }
+];
