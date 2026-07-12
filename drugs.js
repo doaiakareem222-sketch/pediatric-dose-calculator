7940cf2b0162999533328c90d5635482cc01b21b
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-12 17:27:46 +0300

    Update drugs.js

--- drugs.js
+++ drugs.js
@@ -21,28 +21,29 @@ const drugs = {
     frequency: "كل 8 ساعات",
     maxDose: 500,
     concentration: 250
-  }.
+  },
+
   azithromycin: {
     name: "Azithromycin",
     mgPerKg: 10,
     frequency: "مرة واحدة يومياً",
     maxDose: 500,
     concentration: 200
-},
+  },
 
-cefixime: {
+  cefixime: {
     name: "Cefixime",
     mgPerKg: 8,
     frequency: "مرة أو مرتين يومياً",
     maxDose: 400,
     concentration: 100
-},
+  },
 
-metronidazole: {
+  metronidazole: {
     name: "Metronidazole",
     mgPerKg: 7.5,
     frequency: "كل 8 ساعات",
     maxDose: 500,
     concentration: 125
-},
+  }
 };
