39ef648e28b0b14822d70d96c7a73a161d0557e5
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-12 17:16:54 +0300

    Update drugs.js

--- drugs.js
+++ drugs.js
@@ -23,3 +23,26 @@ const drugs = {
     concentration: 250
   }
 };
+azithromycin: {
+    name: "Azithromycin",
+    mgPerKg: 10,
+    frequency: "مرة واحدة يومياً",
+    maxDose: 500,
+    concentration: 200
+},
+
+cefixime: {
+    name: "Cefixime",
+    mgPerKg: 8,
+    frequency: "مرة أو مرتين يومياً",
+    maxDose: 400,
+    concentration: 100
+},
+
+metronidazole: {
+    name: "Metronidazole",
+    mgPerKg: 7.5,
+    frequency: "كل 8 ساعات",
+    maxDose: 500,
+    concentration: 125
+},
