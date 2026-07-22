d9cb4cbead792d3abf44c5424f689ee683693d45
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-22 03:37:09 +0300

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,71 @@
+const translations = {
+
+ar: {
+
+title: "🩺 DoseCare",
+
+subtitle: "حاسبة جرعات الأطفال بالاعتماد على الوزن",
+
+searchDisease: "🔍 البحث حسب المرض",
+
+searchDrug: "البحث عن الدواء",
+
+drug: "اسم الدواء",
+
+strength: "تركيز الدواء",
+
+secondDrug: "الدواء الثاني (اختياري)",
+
+age: "العمر (بالسنوات)",
+
+weight: "الوزن (Kg)",
+
+calculate: "🧮 احسب الجرعة",
+
+reset: "🔄 إعادة تعيين",
+
+copy: "📋 نسخ النتيجة",
+
+info: "ℹ️ معلومات الدواء",
+
+favorite: "⭐ إضافة إلى المفضلة",
+
+showFavorites: "📖 عرض المفضلة"
+
+},
+
+en: {
+
+title: "🩺 DoseCare",
+
+subtitle: "Pediatric Dose Calculator",
+
+searchDisease: "🔍 Search Disease",
+
+searchDrug: "Search Drug",
+
+drug: "Drug",
+
+strength: "Drug Strength",
+
+secondDrug: "Second Drug (Optional)",
+
+age: "Age (Years)",
+
+weight: "Weight (Kg)",
+
+calculate: "🧮 Calculate Dose",
+
+reset: "🔄 Reset",
+
+copy: "📋 Copy Result",
+
+info: "ℹ️ Drug Information",
+
+favorite: "⭐ Add to Favorites",
+
+showFavorites: "📖 Show Favorites"
+
+}
+
+};
