15c8748ed0b55f69fd1fe345d383dfec3f71aa3c
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-25 14:01:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -675,3 +675,26 @@ window.addEventListener("DOMContentLoaded", () => {
     updateDashboard();
 
 });
+if ("serviceWorker" in navigator) {
+
+window.addEventListener("load", () => {
+
+navigator.serviceWorker
+
+.register("./sw.js")
+
+.then(() => {
+
+console.log("Service Worker Registered");
+
+})
+
+.catch((err) => {
+
+console.log(err);
+
+});
+
+});
+
+}
