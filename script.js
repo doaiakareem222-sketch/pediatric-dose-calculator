0ff5f868e85b995f42953aa9d688cad0d8b4c539
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-28 01:02:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -350,4 +350,28 @@ function showDrugInfo(drug){
         drug.blackBox || "-";
 
 }
+// ======================================================
+// Side Menu
+// ======================================================
 
+const menuBtn = document.getElementById("menuBtn");
+
+const sideMenu = document.getElementById("sideMenu");
+
+const menuOverlay = document.getElementById("menuOverlay");
+
+menuBtn.addEventListener("click", () => {
+
+    sideMenu.classList.add("open");
+
+    menuOverlay.classList.add("show");
+
+});
+
+menuOverlay.addEventListener("click", () => {
+
+    sideMenu.classList.remove("open");
+
+    menuOverlay.classList.remove("show");
+
+});
