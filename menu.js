9a95606bf2ed1da30b62e9efca10cf5726965f7a
doaiakareem222-sketch <doaiakareem222@gmail.com>
2026-07-29 02:53:43 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -38,3 +38,93 @@ menuOverlay.addEventListener("click", () => {
     menuOverlay.style.display = "none";
 
 });
+// ===============================
+// Language
+// ===============================
+
+let currentLanguage = "en";
+const language = {
+
+    en: {
+
+        language: "🌐 Language",
+
+        appearance: "🌙 Appearance",
+
+        about: "ℹ️ About",
+
+        contact: "📩 Contact & Support"
+
+    },
+
+    ar: {
+
+        language: "🌐 اللغة",
+
+        appearance: "🌙 المظهر",
+
+        about: "ℹ️ حول التطبيق",
+
+        contact: "📩 التواصل والدعم"
+
+    }
+
+};
+// ===============================
+// Update Language
+// ===============================
+
+function updateLanguage(){
+
+    document.getElementById("languageBtn").innerHTML =
+    language[currentLanguage].language;
+
+    document.getElementById("themeBtn").innerHTML =
+    language[currentLanguage].appearance;
+
+    document.getElementById("aboutBtn").innerHTML =
+    language[currentLanguage].about;
+
+    document.getElementById("contactBtn").innerHTML =
+    language[currentLanguage].contact;
+
+}
+// ===============================
+// Language Buttons
+// ===============================
+
+const englishBtn = document.getElementById("englishBtn");
+const arabicBtn = document.getElementById("arabicBtn");
+
+englishBtn.addEventListener("click",()=>{
+
+    currentLanguage = "en";
+
+    localStorage.setItem("language","en");
+
+    updateLanguage();
+
+});
+
+arabicBtn.addEventListener("click",()=>{
+
+    currentLanguage = "ar";
+
+    localStorage.setItem("language","ar");
+
+    updateLanguage();
+
+    // ===============================
+// Load Saved Language
+// ===============================
+
+const savedLanguage = localStorage.getItem("language");
+
+if(savedLanguage){
+
+    currentLanguage = savedLanguage;
+
+}
+
+updateLanguage();
+});
